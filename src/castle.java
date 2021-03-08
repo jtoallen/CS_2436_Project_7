479632bf1506a113815e95798f3697b1717bcdce
jtoallen@gmail.com <allenja2@oregonstate.edu>
2021-03-08 09:57:43 -0600

    updates on Dijkstra's. Updates on if choice is quit. update on choice not valid.

--- src/castle.java
+++ src/castle.java
@@ -1,7 +1,9 @@
 //Author: Jason Allen
-//Date:
+//Date: 8 March 2021
 //Project Description:
-
+//Create a complex of connected rooms. Read in the vertices and edges from files
+//Allow the user to navigate the complex by going North, South, East, or West.
+//Implement Dijkstra's algorithm to find the shortest path to a user selected room.
 
 
 import java.io.File;
@@ -10,9 +12,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
-
 public class castle {
-
 
     static ArrayList<Room> Vertex = new ArrayList<Room>();
     static ArrayList<Room> Path = new ArrayList<Room>();
@@ -24,8 +24,7 @@ public class castle {
         File file = new File("vertex.txt");
         Scanner infile = new Scanner(file);
         String Input = "";
-        while (infile.hasNextLine())
-        {
+        while (infile.hasNextLine()) {
             Input = infile.nextLine();
             Vertex.add(new Room(Input));
         }
@@ -35,39 +34,36 @@ public class castle {
         file = new File("edge.txt");
         infile = new Scanner(file);
         String From, Direction, To;
-        int Count=0;
-        while (infile.hasNext())
-        {
+        int Count = 0;
+        while (infile.hasNext()) {
             Count++;
             From = infile.next();
             Direction = infile.next();
             To = infile.next();
             // locate From Vertex in ArrayList
             int IndexFrom = 0;
-            while (!Vertex.get(IndexFrom).RoomName.equals(From))
-            {  IndexFrom++;  }
+            while (!Vertex.get(IndexFrom).RoomName.equals(From)) {
+                IndexFrom++;
+            }
             // locate To Vertex in ArrayList
             int IndexTo = 0;
-            while (!Vertex.get(IndexTo).RoomName.equals(To))
-            {  IndexTo++;  }
+            while (!Vertex.get(IndexTo).RoomName.equals(To)) {
+                IndexTo++;
+            }
             // create edge
-            if (Direction.equals("North"))
-            {
+            if (Direction.equals("North")) {
                 Vertex.get(IndexFrom).North = Vertex.get(IndexTo);
                 Vertex.get(IndexTo).South = Vertex.get(IndexFrom);
             }
-            if (Direction.equals("South"))
-            {
+            if (Direction.equals("South")) {
                 Vertex.get(IndexFrom).South = Vertex.get(IndexTo);
                 Vertex.get(IndexTo).North = Vertex.get(IndexFrom);
             }
-            if (Direction.equals("East"))
-            {
+            if (Direction.equals("East")) {
                 Vertex.get(IndexFrom).East = Vertex.get(IndexTo);
                 Vertex.get(IndexTo).West = Vertex.get(IndexFrom);
             }
-            if (Direction.equals("West"))
-            {
+            if (Direction.equals("West")) {
                 Vertex.get(IndexFrom).West = Vertex.get(IndexTo);
                 Vertex.get(IndexTo).East = Vertex.get(IndexFrom);
             }
@@ -89,29 +85,41 @@ public class castle {
             System.out.println("n e s w = move, f = find path, or q = quit");
             System.out.println("Enter command: ");
             Choice = in.nextLine().charAt(0);
-            if(Choice == 'n' && T.North !=null) {
+            if (Choice == 'n' && T.North != null) {
                 T = T.North;
                 System.out.println("you are in " + T.RoomName);
-            }if(Choice == 'e' && T.East !=null) {
+            }
+            if (Choice == 'e' && T.East != null) {
                 T = T.East;
                 System.out.println("you are in " + T.RoomName);
-            }if(Choice == 's' && T.South !=null) {
+            }
+            if (Choice == 's' && T.South != null) {
                 T = T.South;
                 System.out.println("you are in " + T.RoomName);
-            }if(Choice == 'w' && T.West !=null) {
+            }
+            if (Choice == 'w' && T.West != null) {
                 T = T.West;
                 System.out.println("you are in " + T.RoomName);
             }
+            if (Choice == 'q') {
+                System.out.println("You have chosen to exit the program");
+            }
             if (Choice != 'n' && Choice != 'e' &&
                     Choice != 's' && Choice != 'w'
-                    && Choice != 'f' && Choice != 'q'){
+                    && Choice != 'f' && Choice != 'q') {
                 System.out.println("Invalid choice. Please make a valid entry.");
             }
+//            if (T.West == null || T.East == null ||
+//                    T.North == null || T.South == null) {
+//                System.out.println("There is not a room in that direction");
+//            }
             if (Choice == 'f') {
                 //this will call Dijkstra's to find the shortest path
-                //step through vertex array list to get pointer to index vertex for FROM ROOM
-                //step through vertex array to get pointer for TO Room
-
+                //ask user where they want to travel to
+                //1 step through vertex array list to get pointer to
+                // index vertex for Current/From Room
+                //2 step through vertex array to get
+                // pointer for To Room
 
 //                String roomTo;
 ////                in = new Scanner(System.in);
@@ -121,25 +129,33 @@ public class castle {
 //                Scanner roomTo = in.next();
 //                Vertex.get(IndexFrom).RoomName.equals(roomTo);
 
+//                String roomStart, roomFinish;
+//                roomStart = T.RoomName;
+//                System.out.println(T.RoomName);
+//                roomFinish = in.nextLine();
 
-                Dijkstra(Vertex.get(0), Vertex.get(3));
-                for(int i = 0; i < Path.size(); i++){
-                    System.out.println("The shortest path from " + Path.get(i).RoomName);
+
+                Dijkstra(Vertex.get(0), Vertex.get(18));
+//                System.out.println(Vertex.get(18).RoomName);
+                System.out.println("Shortest path = " + (Path.size() - 1));
+                for (int i = 0; i < Path.size(); i++) {
+                    System.out.println(Path.get(i).RoomName);
                 }
-
+                System.out.println("=================");
+                System.out.println("you are in " + T.RoomName);
             }
         }
-        System.out.println("You have chosen to exit the program");
+//        System.out.println("Invalid entry. Please try again.");
 
     } //ends main
 
-   public static class Room {
+    public static class Room {
         String RoomName;
         Room North, South, East, West;
         boolean Visited;   // used for Dijkstra
         int Distance;      // used for Dijkstra
 
-        Room (String theRoomName) {
+        Room(String theRoomName) {
             RoomName = theRoomName;
         }
 
@@ -147,8 +163,7 @@ public class castle {
 
     public static void Dijkstra(Room Start, Room Finish) {
         // set distance to all rooms (except for Start) to 1000 and visited = false
-        for (int i=0; i<Vertex.size(); i++)
-        {
+        for (int i = 0; i < Vertex.size(); i++) {
             if (Vertex.get(i) == Start)
                 Vertex.get(i).Distance = 0;
             else
@@ -157,24 +172,21 @@ public class castle {
         }
         // Do Dijkstra - find Distance to each room
         Room Temp = Start;
-        while (!Finish.Visited)
-        {
+        while (!Finish.Visited) {
             Temp.Visited = true;
-            if (Temp.North!=null && !Temp.North.Visited && Temp.North.Distance > Temp.Distance+1)
+            if (Temp.North != null && !Temp.North.Visited && Temp.North.Distance > Temp.Distance + 1)
                 Temp.North.Distance = 1 + Temp.Distance;
-            if (Temp.South!=null && !Temp.South.Visited && Temp.South.Distance > Temp.Distance+1)
+            if (Temp.South != null && !Temp.South.Visited && Temp.South.Distance > Temp.Distance + 1)
                 Temp.South.Distance = 1 + Temp.Distance;
-            if (Temp.East!=null && !Temp.East.Visited && Temp.East.Distance > Temp.Distance+1)
+            if (Temp.East != null && !Temp.East.Visited && Temp.East.Distance > Temp.Distance + 1)
                 Temp.East.Distance = 1 + Temp.Distance;
-            if (Temp.West!=null && !Temp.West.Visited && Temp.West.Distance > Temp.Distance+1)
+            if (Temp.West != null && !Temp.West.Visited && Temp.West.Distance > Temp.Distance + 1)
                 Temp.West.Distance = 1 + Temp.Distance;
 
             int Smallest = 1000;
             int SmallestIndex = 0;
-            for (int i=0; i<Vertex.size(); i++)
-            {
-                if (!Vertex.get(i).Visited && Vertex.get(i).Distance < Smallest)
-                {
+            for (int i = 0; i < Vertex.size(); i++) {
+                if (!Vertex.get(i).Visited && Vertex.get(i).Distance < Smallest) {
                     Smallest = Vertex.get(i).Distance;
                     SmallestIndex = i;
                 }
@@ -185,14 +197,13 @@ public class castle {
         // populate Path ArrayList with Rooms of shortest path
         Temp = Finish;
         Path.clear();
-        Path.add(0,Finish);
-        while (Temp != Start)
-        {
+        Path.add(0, Finish);
+        while (Temp != Start) {
             int N = 1000, S = 1000, E = 1000, W = 1000;
-            if (Temp.North != null)  N = Temp.North.Distance;
-            if (Temp.South != null)  S = Temp.South.Distance;
-            if (Temp.East != null)  E = Temp.East.Distance;
-            if (Temp.West != null)  W = Temp.West.Distance;
+            if (Temp.North != null) N = Temp.North.Distance;
+            if (Temp.South != null) S = Temp.South.Distance;
+            if (Temp.East != null) E = Temp.East.Distance;
+            if (Temp.West != null) W = Temp.West.Distance;
             if (N < S && N < E && N < W)
                 Temp = Temp.North;
             else if (S < E && S < W)
@@ -201,7 +212,7 @@ public class castle {
                 Temp = Temp.East;
             else
                 Temp = Temp.West;
-            Path.add(0,Temp);
+            Path.add(0, Temp);
         }
     } //ends Dijkstra's
 
